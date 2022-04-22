9bea5f853fcf26dc374b4c4295e69b0dfebc60ad
JuanSebastianGB <3393@holbertonschool.com>
2022-04-21 20:11:54 -0500

    UPl: Uploading the gived interview exercise

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""determines if all the boxes can be opened."""
+
+
+def checkBoxes(listOfKeys, boxes):
+    """Checking all the keys"""
+    totalBoxes = boxes.__len__()
+    for index in range(totalBoxes):
+        if index in boxes[index]:
+            if listOfKeys.count(index) == 1:
+                return False
+    return True
+
+
+def canUnlockAll(boxes):
+    """Filling a list of keys"""
+    if type(boxes) is not list or boxes.__len__() <= 0:
+        return False
+    listOfKeys = []
+    for box in boxes:
+        for index in box:
+            listOfKeys.append(index)
+    return checkBoxes(listOfKeys, boxes)
