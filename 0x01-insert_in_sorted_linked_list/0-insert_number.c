afa3d5ce29edf6e0908ee8aa61e0a1e9e9873d3a
JuanSebastianGB <3393@holbertonschool.com>
2022-04-27 21:13:19 -0500

    DIR: Uploading dir of the double linked list node insertion

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,12 @@
+#include "lists.h";
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ *
+ * @head: the head of the list
+ * @number: the number to insert
+ * Return: listint_t*
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+}
