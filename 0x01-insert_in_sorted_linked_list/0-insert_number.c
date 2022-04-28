ed3b522ad533889d18a9f50b9b681744c60884dc
JuanSebastianGB <3393@holbertonschool.com>
2022-04-27 22:25:14 -0500

    Finishing exercise

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,5 +1,4 @@
-#include "lists.h";
-
+#include "lists.h"
 /**
  * insert_node - inserts a number into a sorted singly linked list.
  *
@@ -9,4 +8,25 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+    listint_t *current = *head;
+    listint_t *newNode, *currentTemporal;
+
+    if (current == NULL)
+        return (NULL);
+    newNode = malloc(sizeof(listint_t));
+    /* Testing fail in node creation */
+    if (newNode == NULL)
+        return (NULL);
+    newNode->n = number;
+    while (current->next != NULL)
+    {
+        if (current->n <= number && current->next->n >= number)
+        {
+            currentTemporal = current->next;
+            current->next = newNode;
+            newNode->next = currentTemporal;
+        }
+        current = current->next;
+    }
+    return (*head);
 }
