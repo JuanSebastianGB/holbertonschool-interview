99b72ab282a96282afa950bdfb6b300c94aee6a7
JuanSebastianGB <3393@holbertonschool.com>
2022-05-12 21:11:14 -0500

    Refactor and checking logic

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,11 +4,15 @@
 
 def minOperations(n=0):
     """Calculates the minimun quantity of operations"""
+
     if type(n) != int or n <= 0:
         return 0
-    result = 0
-    for i in range(2, n + 1):
-        while n % i == 0:
-            result += i
-            n = n/i
+    result, control = 0, 2
+
+    while n > 1:
+        if n % control != 0:
+            control += 1
+        else:
+            n /= control
+            result += control
     return result
