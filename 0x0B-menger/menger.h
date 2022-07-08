d7066c96bf8a5a757699b1b45d5244c652e025d2
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-07 21:26:17 -0500

    Fix menger header file

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -1,7 +1,6 @@
 #ifndef __MENGER_H__
 #define __MENGER_H__
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +8,6 @@
 #include <time.h>
 #include <sys/time.h>
 
-
 void menger(int level);
 
-#endif // __MENGER_H__
+#endif /*__MENGER_H__*/
