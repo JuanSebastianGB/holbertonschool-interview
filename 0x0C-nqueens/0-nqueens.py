ea70626bd0c84f1ff5625b5e5e3350602d6c48e9
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-21 23:41:45 -0500

    Finishing algorythm

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+""" N queens Interview Question """
+
+
 from typing import List
 import sys
 
@@ -23,26 +26,75 @@ def get_size():
     return n
 
 
-def create_board(board_size: int) -> List[List[str]]:
+def unsafe_position(
+        board: List[List[str]],
+        row: int, col: int,
+        current: int) -> bool:
     """
-    Create a board.
+    If the queen is in the same column, or in the same diagonal,
+    as any other queen, then it's unsafe
+
+    :param board: the current board
+    :type board: List[List[str]]
+    :param row: the row we're currently on
+    :type row: int
+    :param col: the column we're currently trying to place a queen in
+    :type col: int
+    :param current: the current row we are on
+    :type current: int
+    :return: True or False
     """
-    board = [0 * board_size for _ in range(board_size)]
-    print(board)
+
+    if (board[row] == col) or \
+        (board[row] == col - row + current) or\
+            (board[row] == row - current + col):
+        return True
+    return False
 
 
-def solve_n_queens(n: int) -> List[List[str]]:
+def print_board(board, n):
     """
-    Solve the n-queens problem.
+    It takes a board and an n, and returns a list of the
+    coordinates of the queens on the board
 
-    Args:
-        n: The size of the board.
-
-    Returns:
-        A list of solutions.
+    :param board: a list of integers, where each integer
+    represents the column of the queen in that row
+    :param n: the number of queens
     """
+
+    result = []
+
+    for i in range(n):
+        for j in range(n):
+            if j == board[i]:
+                result.append([i, j])
+    print(result)
+
+
+def fill_positions(board, row,  n):
+    """
+    For each row, try each column, and if it's safe, recurse on the next row
+
+    :param board: the chess board
+    :param row: the row we are currently on
+    :param n: the size of the board
+    """
+
+    if row == n:
+        print_board(board, n)
+        return
+
+    for j in range(n):
+        is_safe = True
+        for i in range(row):
+            if unsafe_position(board, i, j, row):
+                is_safe = False
+        if is_safe:
+            board[row] = j
+            fill_positions(board, row + 1, n)
 
 
 if __name__ == '__main__':
     board_size = get_size()
-    create_board(board_size)
+    initial_list = [0] * board_size
+    fill_positions(initial_list, 0, board_size)
