539f6eeb20f598e300f3b4615fa1b41a5079d9f1
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-07-21 22:43:28 -0500

    Starting to solve nqueens algorythm

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from typing import List
+import sys
+
+
+def get_size():
+    """
+    Get the size of the board.
+    """
+    arguments = sys.argv
+    if len(arguments) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+    try:
+        n = int(arguments[1])
+        if n < 4:
+            print('N must be at least 4')
+            sys.exit(1)
+    except ValueError:
+        print('N must be an integer')
+        sys.exit(1)
+    return n
+
+
+def create_board(board_size: int) -> List[List[str]]:
+    """
+    Create a board.
+    """
+    pass
+
+
+def solve_n_queens(n: int) -> List[List[str]]:
+    """
+    Solve the n-queens problem.
+
+    Args:
+        n: The size of the board.
+
+    Returns:
+        A list of solutions.
+    """
+
+
+if __name__ == '__main__':
+    n = get_size()
+    print(n)
