b16e4e68099af33f26463691d49530919f0d3a22
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-08-10 22:59:02 -0500

    Fix: Printing in the required order

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 // Using request to implement a http request to the
 // starWars API
@@ -7,13 +7,12 @@ const request = require('request');
 
 const id = process.argv.splice(2);
 
-const URL = `https://swapi-api.hbtn.io/api/films/${id}/`;
+const URL = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-request(URL, (error, response, body) => {
+request(URL, async (error, response, body) => {
   if (response.statusCode !== 200) console.log(error);
   else {
-    const data = JSON.parse(body);
-    const characters = data.characters;
+    const { characters } = JSON.parse(body);
 
     const charactersList = (urlId) => {
       const createPromise = new Promise((resolve, reject) => {
@@ -25,9 +24,9 @@ request(URL, (error, response, body) => {
       return createPromise;
     };
 
-    characters.map(async (url) => {
-      const character = await charactersList(url);
-      console.log(character.name);
-    });
+    for (let i = 0; i < characters.length; i++) {
+      const result = await charactersList(characters[i]);
+      console.log(result.name);
+    }
   }
 });
