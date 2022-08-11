c7dc470aa5e3eec027d5eae66001734cc789ec54
JuanSebastianGB <cbas@LAPTOP-00R7KJB9.localdomain>
2022-08-10 22:18:16 -0500

    Adding docstring

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
+// Using request to implement a http request to the
+// starWars API
+
 const request = require('request');
 
 const id = process.argv.splice(2);
@@ -7,22 +10,24 @@ const id = process.argv.splice(2);
 const URL = `https://swapi-api.hbtn.io/api/films/${id}/`;
 
 request(URL, (error, response, body) => {
-  if (response.statusCode !== 200) return;
-  const data = JSON.parse(body);
-  const characters = data.characters;
+  if (response.statusCode !== 200) console.log(error);
+  else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
 
-  const charactersList = (urlId) => {
-    const createPromise = new Promise((resolve, reject) => {
-      request(urlId, (error, response, body) => {
-        if (error) reject(error);
-        resolve(JSON.parse(body));
+    const charactersList = (urlId) => {
+      const createPromise = new Promise((resolve, reject) => {
+        request(urlId, (error, response, body) => {
+          if (error) reject(error);
+          resolve(JSON.parse(body));
+        });
       });
-    });
-    return createPromise;
-  };
+      return createPromise;
+    };
 
-  characters.map(async (url) => {
-    const character = await charactersList(url);
-    console.log(character.name);
-  });
+    characters.map(async (url) => {
+      const character = await charactersList(url);
+      console.log(character.name);
+    });
+  }
 });
