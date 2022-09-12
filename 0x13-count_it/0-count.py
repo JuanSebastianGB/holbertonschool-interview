b5d5740e04452da6ebdcdff97cff91260fd80032
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-12 00:43:09 +0000

    last try

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API, parses the title of all
-hot articles,and prints a sorted count of given keywords
-(case-insensitive, delimited by spaces. Javascript should count as javascript,
-but java should not).
+Count it! - Write a recursive function that queries the Reddit API,
+            parses the title of all hot articles, and prints a sorted
+            count of given keywords(case-insensitive, delimited by
+            spaces.
+            Javascript should count as javascript, but java should not).
 """
 
 import requests
