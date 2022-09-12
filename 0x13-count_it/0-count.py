ccb3c8b59fe0fdf890d073358ed91f22b7cc51a3
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-12 00:39:12 +0000

    Incrmenting length of docstring

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-""" counting module reddit """
+"""
+Recursive function that queries the Reddit API, parses the title of all
+hot articles,and prints a sorted count of given keywords
+(case-insensitive, delimited by spaces. Javascript should count as javascript,
+but java should not).
+"""
 
 import requests
 
