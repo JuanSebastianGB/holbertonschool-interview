fb13e112e791ac79fdac50a76083ecd883ce5260
JuanGonzaGB <3393@holbertonstudents.com>
2022-10-09 23:28:05 +0000

    Finishing

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,5 +1,33 @@
 #include "lists.h"
 
-listint_t *find_listint_loop(listint_t *head){
-  return (NULL);
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: pointer to the first node in the singly linked list to check
+ * Return: The address of the node where the loop starts,
+ * or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *turtle = head, *hare = head;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	while (hare->next != NULL && hare->next->next != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+		if (turtle == hare)
+		{
+			turtle = head;
+			while (hare != NULL)
+			{
+				if (turtle == hare)
+					return (turtle);
+				turtle = turtle->next;
+				hare = hare->next;
+			}
+		}
+	}
+	return (NULL);
 }
