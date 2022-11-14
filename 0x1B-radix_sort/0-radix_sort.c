0890189dcdd2bd9ef780ea4eadad21e333f0b021
JuanGonzaGB <3393@holbertonstudents.com>
2022-11-14 00:42:24 +0000

    Finishing

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,55 +1,85 @@
 #include "sort.h"
 
-int current_digit_sort(int *array, ssize_t size, int *tmp, long exp)
-{	ssize_t i;
-	int aux[10] = {0};
-
-	for (i = 0; i < size; i++)
-	{
-		aux[(array[i] / exp) % 10]++, tmp[i] = 0;
-	}
-	for (i = 1; i < 10; i++)
-	{
-		aux[i] += aux[i - 1];
-	}
-	for (i = size - 1; i >= 0; i--)
-	{
-		tmp[--aux[(array[i] / exp) % 10]] = array[i];
-	}
-	for (i = 0; i < size; i++)
-	{
-		array[i] = tmp[i];
-	}
-	return (0);
-}
 /**
- * radix_sort - function that sorts an array of integers
- * in ascending order using the Radix sort algorithm
- * @array: will contain only numbers >= 0
- * @size: size of array
- * Return: expected to print the array each time you increase
- */
+ * swap - Change the greatest with the lowest value
+ * @arr: original array
+ * @digit: array of least significant digits
+ * @mv_idx: array idx to move
+ * @mv_to: array idx to move mv_idx to
+ *
+ * Return: nothing
+ **/
+void swap(int *arr, int *digit, int mv_idx, int mv_to)
+{
+	int tmp = arr[mv_idx];
+	int tmp2 = digit[mv_idx];
+
+	while (mv_idx > mv_to)
+	{
+		arr[mv_idx] = arr[mv_idx - 1];
+		digit[mv_idx] = digit[mv_idx - 1];
+		mv_idx--;
+	}
+	arr[mv_to] = tmp;
+	digit[mv_to] = tmp2;
+}
+
+/**
+ * radix_sort - radix sort
+ *
+ * @array: array of integers t sort
+ * @size: number of elements in array
+ *
+ * Return: nothing
+ **/
 void radix_sort(int *array, size_t size)
 {
-	size_t i;
-	long exp = 1;
-	int *temporal_node, max = INT_MIN;
+	int *lsd, max_digits = 0;
+	int i = -1, j = -1, k, l, tens, digits, size2, num;
 
-	if (!array || size < 2)
+	if (size < 2)
 		return;
-
-	temporal_node = malloc(sizeof(int *) * size);
-	if (!temporal_node)
+	lsd = malloc(sizeof(int) * size);
+	if (!lsd)
 		return;
-
-	for (i = 0; i < size; i++)
-		max = array[i] > max ? array[i] : max;
-
-	while (max / exp > 0)
+	size2 = (int)size;
+	while (i++ < size2 - 1)
 	{
-		current_digit_sort(array, size, temporal_node, exp);
-		print_array(array, size);
-		exp *= 10;
+		digits = 0;
+		num = array[i];
+		while (num > 0)
+		{
+			num /= 10;
+			digits++;
+		}
+		if (digits > max_digits)
+			max_digits = digits;
 	}
-	free(temporal_node);
+	i = 0;
+	tens = 1;
+	while (i++ < max_digits)
+	{
+		if (i == 10)
+			tens /= 10;
+		else
+			tens *= 10;
+		j = -1;
+		while (j++ < size2 - 1)
+		{
+			if (i == 10)
+				lsd[j] = array[j] / tens / 10;
+			else
+				lsd[j] = array[j] % tens / (tens / 10);
+		}
+		k = 0;
+		while (k++ < size2 - 1)
+		{
+			l = -1;
+			while (l++ < k)
+				if (lsd[k] < lsd[l])
+					swap(array, lsd, k, l);
+		}
+		print_array(array, size);
+	}
+	free(lsd);
 }
