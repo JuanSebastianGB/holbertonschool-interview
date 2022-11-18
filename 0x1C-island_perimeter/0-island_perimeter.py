c2436d413d3ea9cd99ced806f24c7b98a35f79e4
JuanGonzaGB <3393@holbertonstudents.com>
2022-11-18 04:59:26 +0000

    Pycodestyle

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -14,10 +14,10 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 if i - 1 < 0 or grid[i - 1][j] == 0:
                     result += 1
-                if grid[i + 1][j] or i + 1 >= len(grid)   == 0:
+                if grid[i + 1][j] or i + 1 >= len(grid) == 0:
                     result += 1
                 if j - 1 < 0 or grid[i][j - 1] == 0:
                     result += 1
-                if grid[i][j + 1] == 0 or j + 1 >= len(grid[i]) :
+                if grid[i][j + 1] == 0 or j + 1 >= len(grid[i]):
                     result += 1
     return result
